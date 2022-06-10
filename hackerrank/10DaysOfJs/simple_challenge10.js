be04536cabc59ddc197f9e7bd9496d7a221df89a
Josias C. Oliveira <sirjcoliveira@outlook.com>
2022-06-09 21:21:05 -0300

    Day 5: Inheritance

---
+++ hackerrank/10DaysOfJs/simple_challenge10.js
@@ -0,0 +1,24 @@
+class Rectangle{
+    constructor(w, h){
+        this.w = w;
+        this.h = h;
+    }
+
+}
+
+Rectangle.prototype.area = function (){
+    return this.h === undefined ? this.w * this.w : this.w * this.h
+};
+
+class Square extends Rectangle{}
+
+if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
+    const rec = new Rectangle(3, 4);
+    const sqr = new Square(3);
+    
+    console.log(rec.area());
+    console.log(sqr.area());
+} else {
+    console.log(-1);
+    console.log(-1);
+}
